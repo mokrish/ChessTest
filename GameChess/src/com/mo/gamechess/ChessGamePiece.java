1b9e7abacfb97c1ff33b01977f5bd777882d8f0a
mohan <krishnamohan.cr@gmail.com>
2016-10-01 20:54:48 +0530

    defined equals and hascode methods

--- GameChess/src/com/mo/gamechess/ChessGamePiece.java
+++ GameChess/src/com/mo/gamechess/ChessGamePiece.java
@@ -38,15 +38,43 @@ public abstract class ChessGamePiece
 	}
 
 	@Override
-	public boolean equals(Object arg0) {
-		// TODO Auto-generated method stub
-		return super.equals(arg0);
+	public boolean equals(Object arg0) 
+	{
+		if(arg0==null)
+		{
+			return false;
+		}
+		
+		if(this==arg0)
+		{
+			return true;
+		}
+		
+		if(arg0 instanceof ChessGamePiece)
+		{
+			ChessGamePiece piece = (ChessGamePiece) arg0;
+			if(piece.getColour().equals(this.getColour()) && piece.getName().equals(this.getName()))
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else 
+		{
+			return false;
+		}
 	}
 
 	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
+	public int hashCode() 
+	{
+		int result=17;
+		result=result*31+this.getName().hashCode();
+		result=result*31+this.getColour().hashCode();
+		return result;
 	}
 	
 }
